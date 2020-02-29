945c339f063a3199bb1e260bac690d353311633a
Celestine <celesclaks@yahoo.com>
2020-02-28 21:28:28 -1200

    Updates

---
+++ precache-manifest.fe039233395bfca379cf00980d4c7b68.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba8b727819b0f1fff411f0ab28ccc86",
+    "url": "/OSFglobal/index.html"
+  },
+  {
+    "revision": "4f8f27521e5277ab66ff",
+    "url": "/OSFglobal/static/css/2.6d3ebfce.chunk.css"
+  },
+  {
+    "revision": "e369d52c63475b012a0c",
+    "url": "/OSFglobal/static/css/main.ce8d17b7.chunk.css"
+  },
+  {
+    "revision": "4f8f27521e5277ab66ff",
+    "url": "/OSFglobal/static/js/2.f9d1e682.chunk.js"
+  },
+  {
+    "revision": "a0f2a4b3841e161b41ece838f2bc2a90",
+    "url": "/OSFglobal/static/js/2.f9d1e682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e369d52c63475b012a0c",
+    "url": "/OSFglobal/static/js/main.31fe8a98.chunk.js"
+  },
+  {
+    "revision": "a306b2ea20a9ce683065",
+    "url": "/OSFglobal/static/js/runtime-main.8252f9b7.js"
+  },
+  {
+    "revision": "f2bae2a5791686e3b70cc9b9f096d8a7",
+    "url": "/OSFglobal/static/media/banner.f2bae2a5.jpg"
+  },
+  {
+    "revision": "d7c20fa20c8467c03132f21a8e1ac236",
+    "url": "/OSFglobal/static/media/dropImg.d7c20fa2.jpg"
+  },
+  {
+    "revision": "019721bed4d0aaf4aea18efc6627fbb3",
+    "url": "/OSFglobal/static/media/img1.019721be.jpg"
+  },
+  {
+    "revision": "10cab95cee08d07a9fe8e2340d842aa3",
+    "url": "/OSFglobal/static/media/img2.10cab95c.jpg"
+  },
+  {
+    "revision": "64b05e7a19bdfdea73e4db3a7035ec5d",
+    "url": "/OSFglobal/static/media/img3.64b05e7a.jpg"
+  },
+  {
+    "revision": "643cd82c1b30413f23c94814660da09e",
+    "url": "/OSFglobal/static/media/img4.643cd82c.jpg"
+  },
+  {
+    "revision": "0b6611fa250d4b791646055976a57bca",
+    "url": "/OSFglobal/static/media/palmTree.0b6611fa.png"
+  },
+  {
+    "revision": "dce4ec3987489a47c5288309d24ca218",
+    "url": "/OSFglobal/static/media/prod.dce4ec39.jpg"
+  },
+  {
+    "revision": "0e3083cf76fbe6041b3534b9658e2d3d",
+    "url": "/OSFglobal/static/media/prod2.0e3083cf.jpg"
+  },
+  {
+    "revision": "2960ea023118c6e4e985d36459b69e76",
+    "url": "/OSFglobal/static/media/prod3.2960ea02.jpg"
+  },
+  {
+    "revision": "1c225b2c07fec7adb6196cf9ff85cdfe",
+    "url": "/OSFglobal/static/media/prod4.1c225b2c.jpg"
+  },
+  {
+    "revision": "3adfc2762d8c56381c624d088791f146",
+    "url": "/OSFglobal/static/media/prod5.3adfc276.jpg"
+  },
+  {
+    "revision": "c72df77d68afe0886a7bcac30dbf8f3d",
+    "url": "/OSFglobal/static/media/prod6.c72df77d.jpg"
+  },
+  {
+    "revision": "eb5f88da73c04da09a9b0fa36cf99314",
+    "url": "/OSFglobal/static/media/prod7.eb5f88da.jpg"
+  },
+  {
+    "revision": "4abbbf04004e19083a5bd25ecd6c5945",
+    "url": "/OSFglobal/static/media/prod8.4abbbf04.jpg"
+  },
+  {
+    "revision": "48f537a480a65021079f065708ac2b66",
+    "url": "/OSFglobal/static/media/sale03.48f537a4.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/OSFglobal/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/OSFglobal/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/OSFglobal/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/OSFglobal/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "97edac89cf98f3bd668901dddf54a47c",
+    "url": "/OSFglobal/static/media/sliderImg.97edac89.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e9e5f50d65f1cbaaf04912cfe866dc9.js"
+  "/OSFglobal/precache-manifest.fe039233395bfca379cf00980d4c7b68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OSFglobal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
